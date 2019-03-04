947bafc0c1e7874b998be6cc7442a399db50c9d8
czerwony03 <m.w.033@wp.pl>
2019-03-04 23:40:01 +0100

    Added test model.

---
+++ database/migrations/2019_03_04_223451_create_tests_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('name');
+            $table->string('code');
+        });
+
+        DB::table('tests')->insert(
+            array(
+                'name' => 'Test kolorów stroop\'a',
+                'code' => 'stroop'
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tests');
+    }
+}
