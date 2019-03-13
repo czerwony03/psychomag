a1c948ad5efec95c8bfc90bddd012da1eece6193
czerwony03 <m.w.033@wp.pl>
2019-03-13 19:41:47 +0100

    Added instruction for stroop lvl1.

--- resources/views/tests/stroop/lvl1.blade.php
+++ resources/views/tests/stroop/lvl1.blade.php
@@ -11,7 +11,20 @@
 
                     <div class="card-body d-flex justify-content-around align-items-center">
                         <div class="">
-                            <h1 id="stroop-test-msg">Naciśnij start aby rozpocząć test!</h1>
+                            <h1 id="stroop-test-msg">
+                                <div class="d-flex flex-column" style="font-size:0.5em;">
+                                    <span>
+                                        Na ekranie pojawią się nazwy kolorów zapisane czarną czcionką.<br/>
+                                        Twoim zadaniem jest wciśnięcie klawisza <strong>Z</strong>-zielony, <strong>C</strong>-czerwony, <strong>B</strong>-biały, <strong>N</strong>-niebieski w zależności od tego, jaki kolor przeczytasz.<br/><br/>
+                                    </span>
+                                    <span class="text-center w-100">
+                                        Na przykład gdy na ekranie pojawi się:<br/>CZERWONY<br/>To Twoim zadaniem będzie wciśnięcie klawisza C<br/><br/><br/>
+                                    </span>
+                                    <span>
+                                        Wykonaj zadanie najszybciej jak potrafisz! Do dzieła!
+                                    </span>
+                                </div>
+                            </h1>
                         </div>
                     </div>
 
