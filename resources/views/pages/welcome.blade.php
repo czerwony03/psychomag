af2e6332fdb91e41f54749e0a02281d16019d133
Mariusz Wroński <mwronski@opgksoftware.com>
2019-04-03 14:42:22 +0200

    Added welcome text to welcome page.

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,97 +1,30 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{ config('app.name') }}</title>
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Cześć!</div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-        <!-- Styles -->
+                    <div class="card-body">
+                        <p>Jestem studentką IV roku psychologii Uniwersytetu Marii Curie-Skłodowskiej i prowadzę badania do pracy magisterskiej.<br/>
+                            Poruszam w niej tematykę funkcjonowania poznawczego młodych dorosłych.<br/>
+                            Bardzo Cię proszę o pomoc! Poświęć mi około 20 minut.<br/>
+                            Na początku uzupełnisz kwestionariusz składający się z 21 pytań, następnie zagrasz w kilka „gier”, na zakończenie ponownie odpowiesz na 8 pytań.<br/>
+                            Jestem pewna, że z Twoją pomocą nazbieram odpowiednią liczbę osób badanych!<br/>
+                            <br/><strong>Badanie jest CAŁKOWICIE ANONIMOWE i dobrowolne, możesz z niego zrezygnować, w każdym momencie jego trwania.</strong><br/>
+                            <br/>Z góry dziękuję,<br/>
+                            Ewelina
+                        </p>
 
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ route('auth.home') }}">Panel</a>
-                    @else
-                        <a href="{{ route('login') }}">Zaloguj się</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Zarejestruj się</a>
+                        <a href="{{route('poll_view')}}"><button class="btn btn-success"><h1>START</h1></button></a>
+                        @if(Session::has('tester_uuid'))
+                            <a href="{{route('test.next')}}"><button class="btn btn-info"><h1>KONTYNUUJ</h1></button></a>
                         @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    {{ config('app.name') }}
-                </div>
-
-                <div class="links">
-                    <a href="{{route('poll_view')}}"><button class="btn btn-success"><h1>START</h1></button></a>
-                    @if(Session::has('tester_uuid'))
-                        <a href="{{route('test.next')}}"><button class="btn btn-info"><h1>KONTYNUUJ</h1></button></a>
-                    @endif
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+@endsection
