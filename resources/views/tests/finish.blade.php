49d7600f8f53cf51e18053fe52ce8321b4104faa
Mariusz Wroński <mwronski@opgksoftware.com>
2019-04-05 12:45:12 +0200

    Locked access to results for guests.

--- resources/views/tests/finish.blade.php
+++ resources/views/tests/finish.blade.php
@@ -1,68 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col">
-            <div class="card w-100 mb-3">
-                <div class="card-header bg-success">Zakończenie</div>
-                <div class="card-body">
-                    Bardzo dziękuje za wzięcie udziału w badaniu!
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-            <div class="card w-100 mb-3">
-                <div class="card-header bg-info">Dane użytkownika</div>
-                <div class="card-body">
-                    <ul>
-                        <li>ID: {{$tester->id}}</li>
-                        <li>UUID: {{$tester->uuid}}</li>
-                        <li>Wykonane testy: {{$tester->tests->count()}}/{{$tests->count()}}</li>
-                        <li>Utworzony: {{$tester->created_at}}</li>
-                        <li>Zaktualizowany: {{$tester->updated_at}}</li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        @forelse($testsResults as $test)
-            <div class="col">
-                <div class="card w-100 mb-3">
-                    <div class="card-header">{{ $test->name }}</div>
-                    <div class="card-body">
-                        <ul>
-                            @php
-                                $jsonDecoded = json_decode($test->pivot->result);
-                            unset($jsonDecoded->data);
-                            @endphp
-                            @foreach($jsonDecoded as $key=>$val)
-                                <li>
-                                    {{$key}}:
-                                    @if(is_array($val))
-                                        <ul>
-                                            @foreach($val as $val2)
-                                                <li>{{$val2}}</li>
-                                            @endforeach
-                                        </ul>
-                                    @else
-                                        {{$val}}
-                                    @endif
-                                </li>
-                            @endforeach
-                        </ul>
+    <div class="container">
+        @if(Auth::guest())
+            <div class="row">
+                <div class="col">
+                    <div class="card w-100 mb-3">
+                        <div class="card-header bg-success">Zakończenie</div>
+                        <div class="card-body">
+                            Bardzo dziękuje za wzięcie udziału w badaniu!
+                        </div>
                     </div>
                 </div>
             </div>
-            @if($loop->iteration % 2)
-                <div class="w-100"></div>
-            @endif
-        @empty
-            <div class="alert alert-danger">
-                Brak wyników
+        @else
+            <div class="row">
+                <div class="col">
+                    <div class="card w-100 mb-3">
+                        <div class="card-header bg-info">Dane użytkownika</div>
+                        <div class="card-body">
+                            <ul>
+                                <li>ID: {{$tester->id}}</li>
+                                <li>UUID: {{$tester->uuid}}</li>
+                                <li>Wykonane testy: {{$tester->tests->count()}}/{{$tests->count()}}</li>
+                                <li>Utworzony: {{$tester->created_at}}</li>
+                                <li>Zaktualizowany: {{$tester->updated_at}}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                @forelse($testsResults as $test)
+                    <div class="col">
+                        <div class="card w-100 mb-3">
+                            <div class="card-header">{{ $test->name }}</div>
+                            <div class="card-body">
+                                <ul>
+                                    @php
+                                        $jsonDecoded = json_decode($test->pivot->result);
+                                        unset($jsonDecoded->data);
+                                    @endphp
+                                    @foreach($jsonDecoded as $key=>$val)
+                                        <li>
+                                            {{$key}}:
+                                            @if(is_array($val))
+                                                <ul>
+                                                    @foreach($val as $val2)
+                                                        <li>{{$val2}}</li>
+                                                    @endforeach
+                                                </ul>
+                                            @else
+                                                {{$val}}
+                                            @endif
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    @if($loop->iteration % 2)
+                        <div class="w-100"></div>
+                    @endif
+                @empty
+                    <div class="alert alert-danger">
+                        Brak wyników
+                    </div>
+                @endforelse
             </div>
-        @endforelse
+        @endif
     </div>
-</div>
 @endsection
